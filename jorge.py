12548b6f356b0bb2a5b1c31a158cd8f9b2e9dabe
Alvaro Palero <alvaro.palero@utec.edu.pe>
2023-08-16 02:32:32 -0500

    agregue a jorge

---
+++ jorge.py
@@ -0,0 +1,5 @@
+def resta():
+    a=input("Numero 1: ")
+    b=input("Numero 2: ")
+    f=int(a)-int(b)
+    print(f)
--- pepe.py
+++ pepe.py
@@ -1,3 +1,5 @@
 import lala as ll
+import jorge as jg
 if __name__=='__main__':
     ll.sumar()
+    jg.resta()
